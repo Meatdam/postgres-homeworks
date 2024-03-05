4399da43d884f0c193932b7b23662c94848daf6d
Kuzkin Ilya <heartsong_@mail.ru>
2024-03-05 15:27:33 +0500

    добавление файла config.py для корректного пути к csv файлам

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+ROOT_DIR = os.path.dirname(__file__)
+CUSTOMERS = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'customers_data.csv')
+EMPLOYEES = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'employees_data.csv')
+ORDERS = os.path.join(ROOT_DIR, 'homework-1', 'north_data', 'orders_data.csv')
